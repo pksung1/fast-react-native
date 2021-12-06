422ce52cd5c75121d67abef609bf44054619085a
seonpark <pksung1@gmail.com>
2021-12-07 01:07:58 +0900

    feat: i18n android fixed

--- template/app/i18n/index.ts
+++ template/app/i18n/index.ts
@@ -3,9 +3,9 @@ import {initReactI18next} from 'react-i18next';
 import en from './en';
 import ko from './ko';
 import RNLocalize from 'react-native-localize';
-
 if (!i18next.isInitialized) {
   i18next.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
     lng: RNLocalize.getLocales()[0].languageCode,
     fallbackLng: 'en',
     debug: __DEV__,
