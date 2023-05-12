40b280a5b95569d60b9471aa78d36ba88ae083b4
vidi <pksung1@gmail.com>
2023-05-12 15:26:15 +0900

    feat: RN07버전 템플릿으로 변경작업

--- template.config.js
+++ template.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "ProjectName",
+  placeholderName: "rn07",
 
   titlePlaceholder: 'Hello Seonpark Template',
   // Directory with template
-  templateDir: "./template",
+  templateDir: "./rn07",
 
   // Path to script, which will be executed after init
   // postInitScript: "./script.js"
